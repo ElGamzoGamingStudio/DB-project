6418516d1c494715f4658819eccc84b7324e2ea6
dshpet <dshpet@gmail.com>
2014-02-26 23:45:35 +0200

    Task + notes
    Sync tasks. #pleasenogui

--- PathToSuccess/PathToSuccess/Models/Step.cs
+++ PathToSuccess/PathToSuccess/Models/Step.cs
@@ -8,5 +8,15 @@ namespace PathToSuccess.Models
 {
     public class Step : ICompletable
     {
+        #region Fields
+        public bool Completed { get; private set; }
+        #endregion
+
+        #region Methods
+        public void Complete()
+        {
+            Completed = false;
+        }
+        #endregion
     }
 }
