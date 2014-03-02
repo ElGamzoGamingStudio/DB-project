027d6e38f6442bc04b77623868c92d483f97a402
dshpet <dshpet@gmail.com>
2014-03-02 22:57:38 +0200

    done Task + TaskInitializer
    may be not full and needed to be changed later

--- PathToSuccess/PathToSuccess/Models/Task.cs
+++ PathToSuccess/PathToSuccess/Models/Task.cs
@@ -8,21 +8,50 @@ namespace PathToSuccess.Models
 {
     public class Task : CompletableItem//задача
     {
-        public DateTime DeadLine;
-        public int ID
+        //private Task parentTask;
+        public List<CompletableItem> RelatedSteps { get; private set; }
+
+
+        public Task(CompletionCriteria criteria, DateTime startTime, DateTime deadLine, TimeSpan timeToDo, Importance i, Urgency u) : 
+            base(criteria, startTime, deadLine, timeToDo, i, u)
         {
-            private set;
-            get;
+            RelatedSteps = new List<CompletableItem>();
         }
 
-        public Importance Importance;
-        public Urgency Urgency;
-        CompletionCriteria completionCriteria;
-        TimeRule timeRule;
-
-        public void Complete()
+        public Task(CompletionCriteria criteria, DateTime startTime, DateTime deadLine, TimeSpan timeToDo, Importance i,
+            Urgency u, List<CompletableItem> related) :
+                base(criteria, startTime, deadLine, timeToDo, i, u)
         {
-            throw new NotImplementedException();
+            RelatedSteps = related;
         }
+
+
+        public void AddStep(CompletableItem step)
+        {
+            if (!CheckTypeValidation(step)) return; //type of adding doesnt match type of list elements
+
+            RelatedSteps.Add(step);
+            OnStepAdd();
+        }
+
+        public void RemoveStep(CompletableItem step)
+        {
+            RelatedSteps.Remove(step);
+            OnStepRemove();
+        }
+
+        /// <summary>
+        /// checks type to be identical as the type of the whole list
+        /// </summary>
+        /// <param name="toCheck"></param>
+        /// <returns></returns>
+        private bool CheckTypeValidation(CompletableItem toCheck)
+        {
+
+            return !RelatedSteps.Any() || RelatedSteps[0].GetType() == toCheck.GetType();
+        }
+
+        public CompletableItemEvent OnStepAdd;
+        public CompletableItemEvent OnStepRemove;
     }
 }
--- PathToSuccess/PathToSuccess/Models/TaskInitializer.cs
+++ PathToSuccess/PathToSuccess/Models/TaskInitializer.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace PathToSuccess.Models
 {
-    class TaskInitializer
+    public static class TaskInitializer
     {
+        public static Task CreateTask(CompletionCriteria criteria, DateTime startTime, DateTime deadLine, TimeSpan timeToDo, Importance i, Urgency u)
+        {
+            return new Task(criteria, startTime, deadLine, timeToDo, i, u);
+        }
+
+        /// <summary>
+        /// realted completableItem list is used as a reference!
+        /// </summary>
+        /// <param name="criteria"></param>
+        /// <param name="startTime"></param>
+        /// <param name="deadLine"></param>
+        /// <param name="timeToDo"></param>
+        /// <param name="i"></param>
+        /// <param name="u"></param>
+        /// <param name="related">reference, not a copy</param>
+        /// <returns></returns>
+        public static Task CreateTask(CompletionCriteria criteria, DateTime startTime, DateTime deadLine,
+            TimeSpan timeToDo, Importance i, Urgency u, List<CompletableItem> related)
+        {
+            return new Task(criteria, startTime, deadLine, timeToDo, i, u, related);
+        }
     }
 }
