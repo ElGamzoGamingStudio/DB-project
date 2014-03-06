8deb55ea5cb742131aa9b62b883b5c151f0a275a
ATATtheImperial <vlad.groovy@gmail.com>
2014-03-06 18:30:41 +0200

    User.cs changes
    added some stuff in User class

--- PathToSuccess/PathToSuccess/Models/User.cs
+++ PathToSuccess/PathToSuccess/Models/User.cs
@@ -8,6 +8,62 @@ namespace PathToSuccess.Models
 {
     class User
     {
-        
+        private string _login;
+        public string Login
+        {
+            get 
+            {
+                return _login;
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        private string _surname;
+        public string Surname
+        {
+            get 
+            {
+                return _surname;
+            }
+        }
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get 
+            {
+                return _dateOfBirth;
+            }
+        }
+        private DateTime _dateOfRegistration;
+        private int _password;
+
+        public User(string login, string name, string sname, DateTime birth, string pass)
+        {
+            this._login = login;
+            this._name = name;
+            this._surname = sname;
+            this._dateOfBirth = birth;
+            this._dateOfRegistration = DateTime.Now;
+            this._password = pass.GetHashCode();
+        }
+        public User(string login, string name, string sname, DateTime birth, DateTime registration, int pass)
+        {
+            this._login = login;
+            this._name = name;
+            this._surname = sname;
+            this._dateOfBirth = birth;
+            this._dateOfRegistration = registration;
+            this._password = pass;
+        }
+        public bool ComparePass(string pass)
+        {
+            return _password == pass.GetHashCode();
+        }
     }
 }
