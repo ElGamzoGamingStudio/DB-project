d1b53d67c6008c2d3955bf21a71a156de701b855
Vlad Humniuk <vlad.groovy@gmail.com>
2014-05-04 16:56:51 +0300

    Some TimeBinding stuff
    done

--- PathToSuccess/PathToSuccess/Models/TimeBinding.cs
+++ PathToSuccess/PathToSuccess/Models/TimeBinding.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PathToSuccess.Models
 {
+    [Table("time_binding", Schema = "public")]
     public class TimeBinding
     {
+        [Key]
+        [Column("tb_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int Id { get; set; }
+
+        [Column("step_id")]
+        public int StepId { get; set; }
+
+        [Column("time")]
+        public DateTime Time { get; set; }
+
+        [Column("day")]
+        public int Day { get; set; }
+
+        [Column("month")]
+        public int Month { get; set; }
+
+        [Column("year")]
+        public int Year { get; set; }
+
+        public TimeBinding(int id,int stepId,DateTime time, int day, int month, int year)
+        {
+            Id = id;
+            StepId = stepId;
+            Time = time;
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public static void CreateTimeBinding(TimeBinding timeBinding)
+        {
+            var set = DAL.SqlRepository.DBContext.GetDbSet<TimeBinding>();
+
+            set.Add(timeBinding);
+            DAL.SqlRepository.DBContext.SaveChanges();
+        }
+        public static void DeleteTimeBinding(TimeBinding timeBinding)
+        {
+            var set = DAL.SqlRepository.DBContext.GetDbSet<TimeBinding>();
+            var tb = set.Find(timeBinding.Id);
+            if (tb != null)
+            {
+                set.Remove(timeBinding);
+                DAL.SqlRepository.DBContext.SaveChanges();
+            }
+        }
     }
 }
