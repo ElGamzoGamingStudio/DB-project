8f61efe5ecbad6166b7a69497d2acb779111f29e
Nuvar <nuvar2011@gmail.com>
2014-05-05 20:00:40 +0300

    Connection string from file
    bin\Debug\DbConnectionInfo.xml contains db connection info
    removed SqlRepository from .gitignore

---
+++ PathToSuccess/PathToSuccess/DAL/SqlRepository.cs
@@ -0,0 +1,22 @@
+using Npgsql;
+using System.Xml.Linq;
+
+namespace PathToSuccess.DAL
+{
+    public static class SqlRepository
+    {
+        public static Context DBContext { get; private set; }
+
+        public static void Initialize()
+        {
+            var doc = XDocument.Load("DbConnectionInfo.xml");
+            string connectionString = "Server=" + doc.Root.Attribute("ip").Value + ";Port=" + doc.Root.Attribute("port").Value +
+                                      ";User Id=" + doc.Root.Attribute("id").Value + ";Password=" + doc.Root.Attribute("pass").Value +
+                                      ";Database=" + doc.Root.Attribute("dbname").Value + ";";
+            var conn = new NpgsqlConnection(connectionString); //5432
+            conn.Open();
+
+            DBContext = new Context(conn);
+        }
+    }
+}
