5457d6110dd6004e4403cddcc18257135b551394
dshpet <dshpet@gmail.com>
2014-05-07 19:00:56 +0300

    added to schedule visualiser

--- PathToSuccess/PathToSuccess/ScheduleVisualiser.xaml.cs
+++ PathToSuccess/PathToSuccess/ScheduleVisualiser.xaml.cs
@@ -27,7 +27,7 @@ namespace PathToSuccess
             InitializeComponent();
 
             ScheduleListView.Width = Width - 150;
-            ScheduleListView.Height = Height /3;
+            ScheduleListView.Height = Height / 3;
             Edit.Width = Add.Width = Remove.Width = Width - ScheduleListView.Width - 20;
             Edit.Height = Add.Height = Remove.Height = Remove.Width / 2;
             MondayPanel.MaxWidth =
@@ -41,7 +41,7 @@ namespace PathToSuccess
                                             TuesdayPanel.Width =
                                                 WednesdayPanel.Width =
                                                     ThursdayPanel.Width =
-                                                        FridayPanel.Width = SaturdayPanel.Width = SundayPanel.Width = ScheduleListView.Width/7;
+                                                        FridayPanel.Width = SaturdayPanel.Width = SundayPanel.Width = ScheduleListView.Width / 7;
 
 
 
@@ -61,6 +61,43 @@ namespace PathToSuccess
                 ScheduleListView.Items.Add(o);
             }
 
+            ScheduleListView.SelectionChanged += SelectionChanged;
+
+            ScheduleListView.SelectedItem = ScheduleListView.Items[0];
+            var item = (ScheduleListView.SelectedItem as Models.Schedule);
+            MondayFrom.Text = item.MondayInterval != null ? item.MondayInterval.BeginTime.ToString() : "";
+            MondayTo.Text = item.MondayInterval != null ? item.MondayInterval.EndTime.ToString() : "";
+            TuesdayFrom.Text = item.TuesdayInterval != null ? item.TuesdayInterval.BeginTime.ToString() : "";
+            TuesdayTo.Text = item.TuesdayInterval != null ? item.TuesdayInterval.EndTime.ToString() : "";
+            WednesdayFrom.Text = item.WednesdayInterval != null ? item.WednesdayInterval.BeginTime.ToString() : "";
+            WednesdayTo.Text = item.WednesdayInterval != null ? item.WednesdayInterval.EndTime.ToString() : "";
+            ThursdayFrom.Text = item.ThursdayInterval != null ? item.ThursdayInterval.BeginTime.ToString() : "";
+            ThursdayTo.Text = item.ThursdayInterval != null ? item.ThursdayInterval.EndTime.ToString() : "";
+            FridayFrom.Text = item.FridayInterval != null ? item.FridayInterval.BeginTime.ToString() : "";
+            FridayTo.Text = item.FridayInterval != null ? item.FridayInterval.EndTime.ToString() : "";
+            SaturdayFrom.Text = item.SaturdayInterval != null ? item.SaturdayInterval.BeginTime.ToString() : "";
+            SaturdayTo.Text = item.SaturdayInterval != null ? item.SaturdayInterval.EndTime.ToString() : "";
+            SundayFrom.Text = item.SundayInterval != null ? item.SundayInterval.BeginTime.ToString() : "";
+            SundayTo.Text = item.SundayInterval != null ? item.SundayInterval.EndTime.ToString() : "";
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (ScheduleListView.SelectedItem as Models.Schedule);
+            MondayFrom.Text = item.MondayInterval != null ? item.MondayInterval.BeginTime.ToString() : "";
+            MondayTo.Text = item.MondayInterval != null ? item.MondayInterval.EndTime.ToString() : "";
+            TuesdayFrom.Text = item.TuesdayInterval != null ? item.TuesdayInterval.BeginTime.ToString() : "";
+            TuesdayTo.Text = item.TuesdayInterval != null ? item.TuesdayInterval.EndTime.ToString() : "";
+            WednesdayFrom.Text = item.WednesdayInterval != null ? item.WednesdayInterval.BeginTime.ToString() : "";
+            WednesdayTo.Text = item.WednesdayInterval != null ? item.WednesdayInterval.EndTime.ToString() : "";
+            ThursdayFrom.Text = item.ThursdayInterval != null ? item.ThursdayInterval.BeginTime.ToString() : "";
+            ThursdayTo.Text = item.ThursdayInterval != null ? item.ThursdayInterval.EndTime.ToString() : "";
+            FridayFrom.Text = item.FridayInterval != null ? item.FridayInterval.BeginTime.ToString() : "";
+            FridayTo.Text = item.FridayInterval != null ? item.FridayInterval.EndTime.ToString() : "";
+            SaturdayFrom.Text = item.SaturdayInterval!= null ? item.SaturdayInterval.BeginTime.ToString() : "";
+            SaturdayTo.Text = item.SaturdayInterval != null ? item.SaturdayInterval.EndTime.ToString() : "";
+            SundayFrom.Text = item.SundayInterval != null ? item.SundayInterval.BeginTime.ToString() : "";
+            SundayTo.Text = item.SundayInterval!= null ? item.SundayInterval.EndTime.ToString() : "";
         }
     }
 }
