b76de0d593b16722dadd3503b21c5cd9e234149d
dshpet <dshpet@gmail.com>
2014-05-07 17:20:23 +0300

    git powadi

--- PathToSuccess/PathToSuccess/ScheduleVisualiser.xaml.cs
+++ PathToSuccess/PathToSuccess/ScheduleVisualiser.xaml.cs
@@ -26,24 +26,29 @@ namespace PathToSuccess
         {
             InitializeComponent();
 
-            ScheduleListView.Width = Width - 50;
+            ScheduleListView.Width = Width - 150;
             ScheduleListView.Height = Height - 50;
+            Edit.Width = Add.Width = Remove.Width = Width - ScheduleListView.Width - 20;
+            Edit.Height = Add.Height = Remove.Height = Remove.Width / 2;
+
+
+
             var objCollection = SqlRepository.Schedules; //input smthing
-            
             var obj = objCollection.Cast<Models.Schedule>().FirstOrDefault();
-            var objType = typeof (Models.Schedule);
+            var objType = typeof(Models.Schedule);
             var gridView = new GridView();
+
             ScheduleListView.View = gridView;
 
-            foreach (var prop in objType.GetProperties())
+            foreach (var prop in objType.GetProperties().Where(prop => prop.PropertyType.IsPrimitive || prop.PropertyType == typeof(string) || prop.PropertyType == typeof(DateTime)))
             {
-               gridView.Columns.Add(new GridViewColumn(){Header = prop.Name, DisplayMemberBinding = new Binding(prop.Name)});
+                gridView.Columns.Add(new GridViewColumn() { Header = prop.Name, DisplayMemberBinding = new Binding(prop.Name) });
             }
             foreach (var o in objCollection.Cast<Models.Schedule>())
             {
                 ScheduleListView.Items.Add(o);
             }
-            
+
         }
     }
 }
