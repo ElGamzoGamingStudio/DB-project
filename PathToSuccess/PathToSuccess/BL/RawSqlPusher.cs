a7fbc62228b23c4dc4648a1197a8202b9b53d250
choovuck <nickname.qxo@gmail.com>
2014-05-07 21:10:23 +0300

    added  xml for to init rawsqlpusher

--- PathToSuccess/PathToSuccess/BL/RawSqlPusher.cs
+++ PathToSuccess/PathToSuccess/BL/RawSqlPusher.cs
@@ -4,27 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PathToSuccess.DAL;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using Npgsql;
+using System.Xml.Linq;
+
 
 namespace PathToSuccess.BL
 {
     public static class RawSqlPusher
     {
-        private static Context ctx;
-        private static string connectionString;
 
         public static object PushQuery(string queryText)
         {
-            //ctx.Database.SqlQuery()
+            
             return null;
         }
 
         public static void Initialize()
         {
-            connectionString = "Server=127.0.0.1;Port=3306;User Id=select_only;Database=PathToSuccess;Password=ratdog";
+            var doc = XDocument.Load("RawSqlConnectionInfo.xml");
+            string connectionString = "Server=" + doc.Root.Attribute("ip").Value + ";Port=" + doc.Root.Attribute("port").Value +
+                                      ";User Id=" + doc.Root.Attribute("id").Value + ";Password=" + doc.Root.Attribute("pass").Value +
+                                      ";Database=" + doc.Root.Attribute("dbname").Value + ";";
+
             var conn = new NpgsqlConnection(connectionString);
+    
             conn.Open();
-            ctx = new Context(conn);
+            
+            //var conn = new NpgsqlConnection(connectionString);
+            //conn.Open();
+            //ctx = new Context(conn);
         }
     }
 }
