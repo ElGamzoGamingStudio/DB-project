ef2dbfe54d262122b3597bdd8601b449d580bf82
choovuck <nickname.qxo@gmail.com>
2014-05-07 19:25:24 +0300

    trying to make raw sql work

---
+++ PathToSuccess/PathToSuccess/BL/RawSqlPusher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PathToSuccess.DAL;
+using Npgsql;
+
+namespace PathToSuccess.BL
+{
+    public static class RawSqlPusher
+    {
+        private static Context ctx;
+        private static string connectionString;
+
+        public static object PushQuery(string queryText)
+        {
+            //ctx.Database.SqlQuery()
+            return null;
+        }
+
+        public static void Initialize()
+        {
+            connectionString = "Server=127.0.0.1;Port=3306;User Id=select_only;Database=PathToSuccess;Password=ratdog";
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            ctx = new Context(conn);
+        }
+    }
+}
